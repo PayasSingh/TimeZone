582dfb9b79ba9ffc4be0b9b883d4cc0d4c676f6b
Payas Singh <payassingh@Payass-MacBook-Air-2.local>
2020-09-19 12:47:04 -0600

    Updates

---
+++ precache-manifest.06c890e45ca822964dff812ea9e030be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e78102dbfe88f71758c42be8adfd263",
+    "url": "/%7BGithub-repo-name%7D/index.html"
+  },
+  {
+    "revision": "958f6c19e8c69f020b2c",
+    "url": "/%7BGithub-repo-name%7D/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "441738820e99b6030020",
+    "url": "/%7BGithub-repo-name%7D/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "958f6c19e8c69f020b2c",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.411b3e1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.411b3e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441738820e99b6030020",
+    "url": "/%7BGithub-repo-name%7D/static/js/main.7afdedd7.chunk.js"
+  },
+  {
+    "revision": "042a29f4d559e8247796",
+    "url": "/%7BGithub-repo-name%7D/static/js/runtime-main.a70ab75a.js"
+  }
+]);
